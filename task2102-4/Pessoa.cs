e3c7de4ac7b7d46733ad4a28cbc047be69f6b8db
MarBlueish <6958234@formacao.iefp.pt>
2024-02-21 18:41:05 +0000

    binary formatting

---
+++ task2102-4/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace task2102_4;
+[Serializable]
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    
+}
---
+++ task2102-4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace task2102_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pessoa> pessoas = new List<Pessoa>
+        {
+            new Pessoa{Nome="João", Idade = 25},
+            new Pessoa{Nome = "Maria", Idade = 35},
+            new Pessoa{Nome = "Carlos", Idade = 55}
+        };
+        
+        // caminho arquivo binario
+
+        string caminhoArquivo = "C:\\pessoas.bin";
+        
+        //serializar a lista de objetos para o arquivo binario
+        using (FileStream arquivo = File.Create(caminhoArquivo))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(arquivo,pessoas);
+            
+        }
+        
+        Console.WriteLine("Lista de pessoas serializadas foi armazenado com sucesso!");
+        
+        // leitura de um file escrito em binario
+
+        List<Pessoa> pessoaDesserializadas;
+        using (FileStream arquivo = File.OpenRead(caminhoArquivo))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            pessoaDesserializadas = (List<Pessoa>)formatter.Deserialize(arquivo);
+        }
+        Console.WriteLine("Lista de pessoas deserializadas");
+
+        foreach (Pessoa pessoa in pessoaDesserializadas)
+        {
+            Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}");
+            
+        }
+
+    }
+}
