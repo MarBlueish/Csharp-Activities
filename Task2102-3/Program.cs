724080bad004b86a284c65ca9bc3c0caedd361e1
MarBlueish <6958234@formacao.iefp.pt>
2024-02-21 18:16:26 +0000

    write and read

---
+++ Task2102-3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task2102_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string caminhoArquivo = "C:\\";
+
+        using (StreamWriter escritor = new StreamWriter(caminhoArquivo))
+        {
+            escritor.WriteLine("Olá, mundo");
+            escritor.WriteLine("Este é um arquivo de texto que estamos a testar para escrever um ficheiro txt");
+        }
+        Console.WriteLine("Ficheiro escrito com sucesso");
+        Console.WriteLine("AGORA VAMOS LER O FICHEIRO QUE ACABAMOS DE CRIAR");
+
+        using (StreamReader leitor = new StreamReader(caminhoArquivo))
+        {
+            string linha;
+            while((linha = leitor.ReadLine()) != null)
+            {
+                Console.WriteLine(linha);
+                
+            }
+        }
+    }
+}
