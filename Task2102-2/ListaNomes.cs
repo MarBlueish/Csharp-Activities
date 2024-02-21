93a9a7235d85b4647b23b3bbce6292381c29f5b9
MarBlueish <6958234@formacao.iefp.pt>
2024-02-21 18:00:34 +0000

    Indexing updated
    update on exercise about indexing

---
+++ Task2102-2/ListaNomes.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2102_2;
+
+public class ListaNomes
+{
+    private string[] nomes = new string[5];
+
+    public string this[int index]
+    {
+        get
+        {
+            return nomes[index];
+            
+        }
+        set
+        {
+            nomes[index] = value;
+        }
+    }
+}
---
+++ Task2102-2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2102_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListaNomes lista = new ListaNomes();
+
+        lista[0] = "João";
+        lista[1] = "Maria";
+        lista[2] = "Pedro";
+        
+        Console.WriteLine(lista[1]);
+    }
+}
