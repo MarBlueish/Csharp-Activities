e5b2b2f1599bbf712468abb5a01028cd768faaf9
MarBlueish <6958234@formacao.iefp.pt>
2024-02-22 18:07:36 +0000

    updated

---
+++ ArraysExercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ArraysExercicio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] meuArray = new int[11];
+        int[] invertidoArray = new int[11];
+        meuArray[0] = 1;
+        meuArray[1] = 6;
+        meuArray[2] = 44;
+        meuArray[3] = 243;
+        meuArray[4] = 35;
+        meuArray[5] = 143;
+        meuArray[6] = 1341;
+        meuArray[7] = 54;
+        meuArray[8] = 4132;
+        meuArray[9] = 231;
+        meuArray[10] = 23;
+
+        for (int i = 0; i < meuArray.Length; i++)
+        {
+            Console.WriteLine(meuArray[i]);
+ 
+        }
+        
+        //inversao do array
+        for (int i = 0; i < meuArray.Length; i++)
+        {
+            invertidoArray[i] = meuArray[meuArray.Length - 1 - i];
+        }
+        Console.WriteLine("\nArray Invertido:");
+        
+        for (int i = 0; i < invertidoArray.Length; i++)
+        {
+            Console.WriteLine(invertidoArray[i]);
+        }
+        
+
+
+        
+
+
+    }
+}
---
+++ ListaObjetos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaObjetos
+{
+    class Program
+    {
+        public static List<Aluno> alunos = new List<Aluno>();
+
+        static void Main(string[] args)
+        {
+            alunos.Add(new Aluno { Nome = "João", Idade = 13, Nota = 11.2f });
+            alunos.Add(new Aluno { Nome = "Silva", Idade = 12, Nota = 17.2f });
+            alunos.Add(new Aluno { Nome = "Silveira", Idade = 14, Nota = 19.1f });
+            alunos.Add(new Aluno { Nome = "Sandro", Idade = 13, Nota = 20f });
+            alunos.Add(new Aluno { Nome = "Cassandra", Idade = 12, Nota = 9f });
+
+            MediaDaNota(); // Chame o método aqui
+        }
+
+        static void MediaDaNota()
+        {
+            float somadasNotas = 0;
+
+            foreach (var aluno in alunos)
+            {
+                somadasNotas += aluno.Nota;
+            }
+
+            float mediaNota = somadasNotas / alunos.Count;
+
+            Console.WriteLine("A media das notas dos alunos é: " + mediaNota);
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Nota { get; set; }
+    }
+}
