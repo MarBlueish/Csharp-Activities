308b7c72c96b8876b0e3e2a9bde6a365f7a3a744
MarBlueish <6958234@formacao.iefp.pt>
2024-03-06 23:36:37 +0000

    update

---
+++ DelegatesNcallbacks/Program2.cs
@@ -0,0 +1,49 @@
+﻿namespace DelegatesNcallbacks;
+
+class Program2
+{
+    //1.1 funçao pronta a ser chamada com os mesmos parametros do ponto 2.1
+    static void myCallBackCao(int i)
+    {
+        Console.WriteLine("Ladrar" +i);
+    }
+    
+    //1.2 Outra funçao pronta a ser chamada pelo mesmo delegate que apresenta os mesmos parametros
+    static void myCallBackGato(int i)
+    {
+        Console.WriteLine("Miar" + i);
+    }
+    
+    static void Main2(string[] args)
+    {
+        //1.3 criaçao do objeto da classe Meutrabalho onde esta disponivel o delegate
+        meuTrabalho myObj = new meuTrabalho();
+       
+        //1.4 fazer chamada da instancia com o callback correspondente
+        myObj.FazerSom(myCallBackCao);
+        myObj.FazerSom(myCallBackGato);
+
+    }
+}
+
+class meuTrabalho
+{
+    //2.1 Delegate serve como uma especie de assinatura ou indicação de como irá ser a função enviada por um
+    //parametro, neste caso fico a saber que as funções que chegam chamadas através deste delegate apenas
+    //poderão ter um int como parametro
+    //mycallbackcao e mycallbackgato seguem esses parametros
+    public delegate void myCallBackPointer(int i);
+    
+    //2.2 funcao que ira receber os delegates como parametro, reparar que o tipo de parametro é igual ao delegate
+    //criado na linha anterior
+    public void FazerSom(myCallBackPointer NewObj)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            //2.3 aqui estamos a chamar indiretamente a função mycallbackcao e mycallbackgato, dependendo
+            //do delegate, enviado a quando da chamada desta função no main
+            NewObj(i);
+        }
+    }
+    
+}
---
+++ Events/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Events;
+
+class Publicador
+{
+    //Etapa 1.1- Criar um delegate cujo nome seja
+    //"nomedoevento"+EventHandler
+    public delegate void TrabalhoFeitoEventHandler(object sender, EventArgs args);
+    
+    //Etapa 1.2 - Criar um evento baseado no delegate
+    public event TrabalhoFeitoEventHandler TrabalhoFeito;
+
+    public void ProcessaumTrabalho()
+    {
+        Console.WriteLine("Publicador: Um trabalho foi concluido");
+        //Etapa 1.3- Dispara o evento
+        OnTrabalhoFeito();
+    }
+    // o padrao do c# standard requer que o metodo seja definido como protected virtual.
+    // o nome tambem precisa coicidir com o nome do evento e tera que ter o prefixo
+    //com on.*/
+    protected virtual void OnTrabalhoFeito()
+    {
+        if (TrabalhoFeito != null)
+            TrabalhoFeito(this, EventArgs.Empty);
+        
+    }
+}
+//etapa 2- criar um subscritor
+class Subscriber
+{
+    public void OnTrabalhoFeitoEventHandler(object sender, EventArgs args)
+    {
+        Console.WriteLine("O subscritor foi notificado");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Evento Exemplo");
+        //criar instancia do Editor e do assinante
+        Publicador remetente = new Publicador();
+        Subscriber receptor = new Subscriber();
+        //define o tratamento do evento, aqui subscrevemos o subcritor as notificações do publicado
+        remetente.TrabalhoFeito += receptor.OnTrabalhoFeitoEventHandler;
+        
+        //define o tratamento do evento, aqui deixamos de subscrever o subscritor as notificações do publicado
+        //rementente.Trabalhofeito += receptor.OnTrabalhoFeitoEventHandler;
+        
+        //Invoca
+        remetente.ProcessaumTrabalho();
+        Console.ReadKey();
+
+    }
+}
