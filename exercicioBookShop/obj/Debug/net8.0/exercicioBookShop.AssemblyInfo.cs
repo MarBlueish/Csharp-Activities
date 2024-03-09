d7db448a9f8333c20ffcc6ba5bbe91255d1e12a8
MarBlueish <6958234@formacao.iefp.pt>
2024-03-09 00:35:47 +0000

    update

--- exercicioBookShop/obj/Debug/net8.0/exercicioBookShop.AssemblyInfo.cs
+++ exercicioBookShop/obj/Debug/net8.0/exercicioBookShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicioBookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59022c55ee60747294f5467ecf74492419afc383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7fb199f33f127f10c682e4901736842ad1277d")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicioBookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicioBookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
